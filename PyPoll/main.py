b5bd66d045c316e783cb0c3ab44c4bf5df1dfa09
Dawayne Eason <37123932+tazexpress24@users.noreply.github.com>
2018-06-18 23:00:31 -0400

    WIP
    WIP

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,6 +13,8 @@ votes = []
 counties =  []
 candidates = []
 candidates_list = []
+candidates
+
 with open(csvpath, newline='') as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
@@ -41,62 +43,33 @@ with open(csvpath, newline='') as csvfile:
 
     #The percentage of votes each candidate won
     #for row in csvreader:
-     #       vote = int(row[0])
-      #      votes.append(vote)
+     #   vote = int(row[0])
+     #   votes.append(vote)
     
-       # percent_votes = ((votes[0] / sum(votes))
+    #percent_votes = ((votes[0] / sum(votes))
         
     #percentage of votes = total number votes of candidate / overall total number of votes
 
     #The total number of votes each candidate won
   
- 
-#def winner(input):
- 
-     # convert list of candidates into dictionary
-     # output will be likes candidates = {'A':2, 'B':4}
-     #votes = Counter(input)
-      
-     # create another dictionary and it's key will
-     # be count of votes values will be name of 
-     # candidates
-     #dict = {}
- 
-     #for value in votes.values():
- 
-          # initialize empty list to each key to 
-          # insert candidate names having same 
-          # number of votes 
-          #dict[value] = []
- 
-     #for (key,value) in votes.iteritems():
-     #     dict[value].append(key)
- 
-     # sort keys in descending order to get maximum 
-     # value of votes
-     #maxVote = sorted(dict.keys(),reverse=True)[0]
- 
-     # check if more than 1 candidates have same 
-     # number of votes. If yes, then sort the list
-     # first and print first element
-    #if len(dict[maxVote])>1:
-    #     print sorted(dict[maxVote])[0]
-    #else:
-    #     print dict[maxVote][0]
- 
+    for candidate_number in range(candidates):
+   # print('Candidate {0}:'.format(candidate_number + 1))
+    #name = input('Whats the name of this candidate?\n')
+        candidate_info = (candidate_number + 1, name)
+        candidates.append(candidate_info) 
 
     #The winner of the election based on popular vote.  
     
   
       
             
-    print("Election Results")
-    print("----------------------------")
-    print(f"Total Votes Cast:{totalvotes}" )
-    print(f"List Received Votes:{candidates_list}")
-    #print(f"Percent of Votes:{percent_votes}")
-    #print("Total Votes Candidate won:{}")
-   # print("Winner:{}")
+print("Election Results")
+print("----------------------------")
+print(f"Total Votes Cast:{totalvotes}" )
+print(f"List Received Votes:{candidates_list}")
+#print(f"Percent of Votes:{percent_votes}")
+#print("Total Votes Candidate won:{}")
+# print("Winner:{}")
 
 
 #Election Results
